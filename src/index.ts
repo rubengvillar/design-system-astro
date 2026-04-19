6b1fe1dcdcd1952a9af5c48a7f2abf25742480cf
Ruben Villar <rubenvillar.dev@gmail.com>
2026-04-19 18:07:04 -0300

    Componentes grid layouts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 // src/index.ts
-export { default as Button } from './components/ui/Button.astro';
+// Componentes de Layouts
 export { default as Container } from './components/ui/Container.astro';
+export { default as Grid } from './components/layouts/Grid.astro';
+export { default as GridItem } from './components/layouts/GridItem.astro';
+
+// Componentes de UI visuales atomicos
+export { default as Button } from './components/ui/Button.astro';
 export { default as Card } from './components/ui/Card.astro';
 export { default as Badge } from './components/ui/Badge.astro';

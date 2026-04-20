6d6c48731a0d8a4ae70aef68a26fea4053fd3af8
Ruben Villar <rubenvillar.dev@gmail.com>
2026-04-19 22:59:39 -0300

    Configuracion astro playground

--- playground/astro.config.mjs
+++ playground/astro.config.mjs
@@ -1,5 +1,16 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  vite: {
+    optimizeDeps: {
+      // Obligamos a Vite a incluir tu librería en la pre-optimización
+      include: ['@rubengvillar/design-system-astro'] 
+    },
+    // Opcional: A veces ayuda a evitar problemas de "duplicación" de módulos
+    resolve: {
+      dedupe: ['astro']
+    }
+  }
+});

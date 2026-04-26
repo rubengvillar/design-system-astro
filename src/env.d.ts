461beb8399774dc71fe9cdf708dc9f79444a7c63
Ruben Villar <rubenvillar.dev@gmail.com>
2026-04-26 15:43:30 -0300

    fix(ui): resolve component type errors and declare custom elements

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="astro/client" />
+
+declare namespace astroHTML.JSX {
+  interface IntrinsicElements {
+    'ui-tabs': any;
+    'ui-modal': any;
+    'ui-dropdown': any;
+  }
+}
